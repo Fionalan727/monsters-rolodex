36fae68b26355ff3e87220dbe080eb4cca7f9524
Fionalan727 <lanyufei727@gmail.com>
2019-09-12 19:30:39 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name:'Frankenstein',
+          id:'feikj'
+        },
+        {
+          name:'Dracula',
+          id:'afsd'
+        },
+        {
+          name:'Zombie',
+          id:'sqfdf'
+        }
+
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+       {this.state.monsters.map(monster => (
+           <h1 key={monster.id}>{monster.name}</h1>
+         ))}
+      </div>
+    );
+  }
+}
+
+export default App;
