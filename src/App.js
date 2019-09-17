ba82d349a4d6ab6b854d85f023894bf3bc255a1d
Fionalan727 <lanyufei727@gmail.com>
2019-09-17 18:35:27 -0400

    add search bar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
   componentDidMount(){
@@ -17,6 +18,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <input type='search' placeholder="search monsters" onChange={e => console.log(e)}/>
       <CardList monsters={this.state.monsters}/>
       </div>
     );
