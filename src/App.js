eb2c2253e288d2eb5c6944bf1c09560221b06228
Fionalan727 <lanyufei727@gmail.com>
2019-09-17 19:34:31 -0400

    add search bar as functional component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {CardList} from './components/card-list/card-list.component';
 import './App.css';
+import {SearchBox} from './components/search-box/search-box.component'
 class App extends Component {
   constructor(){
     super();
@@ -15,15 +16,15 @@ class App extends Component {
     .then(res => res.json())
     .then(users => this.setState({monsters: users}));
   }
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLocaleLowerCase()))
     return (
       <div className="App">
-      <input 
-      type='search' 
-      placeholder="search monsters" 
-      onChange={e => this.setState({ searchField : e.target.value})}
+      <SearchBox 
+        placeholder="search monsters" 
+        handleChange={e => this.setState({ searchField : e.target.value})}
       />
       <CardList monsters={filteredMonsters}/>
       </div>
